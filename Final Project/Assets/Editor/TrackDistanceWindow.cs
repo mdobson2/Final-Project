65723757b33d938aed9137f596597e0ae6f0ba7b
mdobson2 <svc-student@RC335G06.student.directory.jccc>
2015-11-07 17:28:54 -0600

    Track 1 in distance window ready for testing
    like the title says

--- Final Project/Assets/Editor/TrackDistanceWindow.cs
+++ Final Project/Assets/Editor/TrackDistanceWindow.cs
@@ -28,7 +28,7 @@ public class TrackDistanceWindow : EditorWindow {
     {
         float track1Distance = 0;
         int track1BezCount = 0;
-        int track1straightCount = 0;
+        int track1StraightCount = 0;
 
         float track2Distance = 0;
         int track2BezCount = 0;
@@ -38,12 +38,41 @@ public class TrackDistanceWindow : EditorWindow {
         int track3BezCount = 0;
         int track3StraightCount = 0;
 
-        foreach(ScriptMovements move in track1.movements)
+        for (int i = 0; i < track1.movements.Count; i++ )
         {
-            switch(move.moveType)
+            Vector3 startPos = new Vector3(0,0,0);
+            if (i == 0)
             {
-
+                startPos = track1.movements[track1.movements.Count - 1].endWaypoint.transform.position;
+            }
+            switch (track1.movements[i].moveType)
+            {
+                case MovementTypes.BEZIER:
+                    Vector3 bezierStart = startPos;
+                    for (int k = 1; k <= 10; k++)
+                    {
+                        Vector3 lineEnd = GetPoint(bezierStart, track1.movements[i].endWaypoint.transform.position, track1.movements[i].curveWaypoint.transform.position, k / 10f);
+                        track1Distance += Vector3.Distance(startPos, lineEnd);
+                        startPos = lineEnd;
+                    }
+                    startPos = track1.movements[i].endWaypoint.transform.position;
+                    track1BezCount++;
+                    break;
+                case MovementTypes.STRAIGHT:
+                    track1Distance += Vector3.Distance(startPos, track1.movements[i].endWaypoint.transform.position);
+                    startPos = track1.movements[i].endWaypoint.transform.position;
+                    track1StraightCount++;
+                    break;
             }
         }
+
+
+    }
+
+    public Vector3 GetPoint(Vector3 start, Vector3 end, Vector3 curve, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return (oneMinusT * oneMinusT * start + 2f * oneMinusT * t * curve + t * t * end);
     }
 }
